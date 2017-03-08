c30b339ea18037e2a70995c96e535b217296b2a6
Chicloon@gmail.com <gfhjkm1982>
2017-03-08 12:53:30 +0900

    initial commit

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import 'babel-core/polyfill';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DumbComponent from './components/dumbcomponent';
+
+ReactDOM.render(
+  <DumbComponent />,
+  document.getElementById('root')
+);
