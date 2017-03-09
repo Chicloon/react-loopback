5cc312f3bb3254d33142e390d59dd9656ea7b448
Chicloon <Chicloon@gmail.com>
2017-03-09 16:30:08 +0900

    minor fix

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ import 'babel-core/polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DumbComponent from './components/dumbcomponent';
+import DumbComponent from './components/DumbComponent';
 
 ReactDOM.render(
   <DumbComponent />,
